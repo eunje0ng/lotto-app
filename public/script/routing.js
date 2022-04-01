5337a1e588e32712e06bec0019ce9cee8e071b32
eunje0ng <100328746+eunje0ng@users.noreply.github.com>
2022-04-01 11:05:00 +0900

    0401로또앱

--- public/script/routing.js
+++ public/script/routing.js
@@ -1,4 +1,10 @@
-// 대상
+// 자바스크립트 변수 : var    vs   let, const(=상수)
+// 차이점!          : old     /    new
+// ECMA Script --> JS Standard! --> ES 5 ==> ES 6 (과도기)
+// 여러분은! var 보다 let, const 사용하세요!
+// 문제점! var는 선언하지 않고도 사용가능하고 또 전역/지역 명확하지 않음.
+
+// 대상 : id요소 camelCase
 const openBTN = document.querySelector("#openSide")
 const closeBTN = document.querySelector("#closeSide")
 const sidePan = document.querySelector("#sidePannel")
--- server.js
+++ server.js
@@ -23,7 +23,14 @@ app.get('/cs', (req, res) => {
 })
 
 app.get('/add_user', (req, res) => {
-  res.sendFile(__dirname+'/public/register.html')
+  res.send("정상적으로 회원가입 되었습니다.")
+})
+
+app.get('/result1', (req, res) => {
+  res.sendFile(__dirname+'/public/result1.html')
+})
+app.get('/result5', (req, res) => {
+  res.sendFile(__dirname+'/public/result5.html')
 })
 
 app.listen(port, () => {
